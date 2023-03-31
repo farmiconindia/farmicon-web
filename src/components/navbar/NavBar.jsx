87f06e1e65472ed9e31f898ce28cbf8f0886bb73
Subhadip <mnhacker2001@gmail.com>
2023-04-01 01:13:29 +0530

    for mobile & tablet

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.scss";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 
 const NavBar = () => {
+
+  const [showNav, setShowNav] = useState(false)
+
   return (
     <>
       <div className="Navbar">
         <header>
-          <div className="brand">
+          <div className="brand" onClick={() => { setShowNav(false) }}>
             <div className="logo"></div>
             <Link style={{ textDecoration: "none" }} to="/">
               <span> Farmicon</span>
@@ -27,7 +32,27 @@ const NavBar = () => {
               <span> Contact Us</span>
             </Link>
           </nav>
+          <div className="burger" onClick={() => { setShowNav(!showNav) }}>
+            {!showNav&&<GiHamburgerMenu />}
+            {!!showNav&&<GrClose />}
+          </div>
         </header>
+        {showNav && <div className="nav-down">
+          <nav className="nav">
+            <Link onClick={() => { setShowNav(!showNav) }} style={{ textDecoration: "none" }} to="/">
+              <span> About</span>
+            </Link>
+            <Link onClick={() => { setShowNav(!showNav) }} style={{ textDecoration: "none" }} to="/services">
+              <span> Services</span>
+            </Link>
+            <Link onClick={() => { setShowNav(!showNav) }} style={{ textDecoration: "none" }} to="/">
+              <span> Blog</span>
+            </Link>
+            <Link onClick={() => { setShowNav(!showNav) }} style={{ textDecoration: "none" }} to="/">
+              <span> Contact Us</span>
+            </Link>
+          </nav>
+        </div>}
       </div>
     </>
   );
