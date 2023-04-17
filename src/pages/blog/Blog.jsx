8a72fce0e80ce780a0baa6b3b5b35888346b5cc1
Subhadip <mnhacker2001@gmail.com>
2023-04-18 04:03:14 +0530

    added voice

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,23 +1,84 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import blogData from "../../store/blogData";
 import "./blog.scss";
 
 const Blog = () => {
+  const { id } = useParams();
+  const blog = blogData.find((blog) => blog.id === Number(id));
+  const [isPlaying, setIsPlaying] = useState(false);
+  const speech = useRef(null);
+  const pRef = useRef(null);
+
+  const handlePlay = () => {
+    if (!isPlaying) {
+      console.log("play");
+      speech.current = new SpeechSynthesisUtterance(pRef.current.textContent);
+      speech.current.lang = "en-IN";
+      window.speechSynthesis.speak(speech.current);
+      setIsPlaying(true);
+    }
+  };
+
+  const handleStop = () => {
+    if (isPlaying) {
+      console.log("stop");
+      window.speechSynthesis.cancel();
+      setIsPlaying(false);
+    }
+  };
+
+  speech.current &&
+    (speech.current.onboundary = (event) => {
+      const charIndex = event.charIndex;
+      const pText = pRef.current.textContent;
+
+      // Find the start and end indices of the current word being spoken
+      let startIndex = charIndex;
+      let endIndex = charIndex;
+      while (startIndex > 0 && !/\s/.test(pText.charAt(startIndex - 1))) {
+        startIndex--;
+      }
+      while (endIndex < pText.length && !/\s/.test(pText.charAt(endIndex))) {
+        endIndex++;
+      }
+
+      // Highlight the current word being spoken
+      pRef.current.innerHTML =
+        pText.substring(0, startIndex) +
+        `<p style="background-color: yellow; display:inline;font-weight:500; text-align:justify">${pText.substring(
+          startIndex,
+          endIndex
+        )}</p>` +
+        pText.substring(endIndex, pText.length);
+    });
+
+  useEffect(() => {
+    return () => {
+      window.speechSynthesis.cancel();
+      setIsPlaying(false);
+      speech.current = null;
+    };
+  }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
-  const { id } = useParams();
-  const blog = blogData.find((blog) => blog.id === Number(id));
   return (
     <div className="blog-container">
-      <span>{blog.title}</span>
+      <span>{blog?.title}</span>
       <div className="blog">
-        <img src={blog.thumbnail} alt={blog.title} />
-        <div className="desc">
-          <p>{blog.description}</p>
+        <div className="btn-grp">
+          <button onClick={handlePlay} disabled={isPlaying}>
+            Play
+          </button>
+          <button onClick={handleStop} disabled={!isPlaying}>
+            Stop
+          </button>
+        </div>
+        <div className="desc" ref={pRef}>
+          <p>{blog?.description}</p>
           <div className="blog-more">
             <span>{blog.date}</span>
           </div>
