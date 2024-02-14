f8e9e4589fc50487321517664136d1792e17ede9
Chirag Bhatia <90553023+chirag3000@users.noreply.github.com>
2024-02-14 11:14:08 +0530

    Update BlogComp.jsx
    addition of blogs images

--- src/components/blog/BlogComp.jsx
+++ src/components/blog/BlogComp.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+mport React from "react";
 import { Link } from "react-router-dom";
 import "./blogComp.scss";
+import image1 from "../../assets/Hydroponics.jpg"
+import image2 from "../../assets/organicFarm.jpg"
+import image3 from "../../assets/Tomatotips.jpg"
+import image4 from "../../assets/grape-cultivation.webp"
+import image5 from "../../assets/Dripirri.webp"
+import image6 from "../../assets/OnionFarming.jpg"
+import image7 from "../../assets/Sugarcane-Farming.jpg"
+import image8 from "../../assets/PomergranateFarming.jpg"
 
 const BlogComp = ({ blog, i, changeLang }) => {
+
+  const images = [image1, image2, image3, image4, image5, image6, image7,image8];
+  const selectedImage = images[i % images.length];
   return (
     <>
       <div className="blog-comp" key={i}>
-        <img
-          src={
-            "https://media.istockphoto.com/id/1296283501/video/young-indian-farmer-talking-on-smartphone-at-field.jpg?s=640x640&k=20&c=c70r74UlGK-3aJhLgF7wNA2tc2LYF044Thoz-CJcgXM="
-          }
+        <img style={{width:"318px", height: "179px"}}
+          src={selectedImage}
           alt=""
         />
         <div className="desc">
@@ -18,7 +27,7 @@ const BlogComp = ({ blog, i, changeLang }) => {
             <Link to={`/blogs/${blog.id}`}>
               {!changeLang ? "Read More" : "और पढ़ें"}
             </Link>
-            <span>{!changeLang ? "July 2021" : "जुलाई 2021"} </span>
+            <span>{!changeLang ? "February 2024" : "फ़रवरी 2024"} </span>
           </div>
         </div>
       </div>
