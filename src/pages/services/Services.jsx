fc3d371f85b083d9ba816e46db6a39ecf01b23cd
farmiconindia <110238011+farmiconindia@users.noreply.github.com>
2024-02-27 18:19:24 +0530

    Update Services.jsx

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -6,7 +6,7 @@ import cropDoc from "../../assets/crop-doc2.png";
 // import cropPrice from "../../assets/crop-price2.png";
 // import cropPrice from "../../assets/Crop-price3.png";
 // import cropHealth from "../../assets/crop-health2.png";
-import cropHealth from "../../assets/crop_health.jpg";
+// import cropHealth from "../../assets/crop_health.jpg";
 import drone from "../../assets/drone2.png";     
 // import drone from "../../assets/hi-tech_drone.jpg";
 import soilHealth from "../../assets/soil-health2.png";
