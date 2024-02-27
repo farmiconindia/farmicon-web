b613f8103f756c5897ee7517165c95518c4c3997
farmiconindia <110238011+farmiconindia@users.noreply.github.com>
2024-02-27 17:59:35 +0530

    Update BlogPag.jsx

--- src/pages/blogPage/BlogPag.jsx
+++ src/pages/blogPage/BlogPag.jsx
@@ -2,18 +2,26 @@ import React, { useEffect } from "react";
 import "./blogPage.scss";
 import blogData from "../../store/blogData";
 import BlogComp from "../../components/blog/BlogComp";
+import AOS from 'aos';
 
 const BlogPage = ({ changeLang }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, 
+      easing: 'ease-in-out', 
+    });
+  }, []);
+
   return (
     <div className="blog-page">
       <div className="blog-page-container">
-        <span>{!changeLang ? "Blogs" : "ब्लॉग"}</span>
+      <span data-aos="fade-up">{!changeLang ? "Blogs" : "ब्लॉग"}</span>
 
-        <div className="blogs">
+        <div className="blogs" data-aos="fade-left">
           {blogData.map((blog, i) => {
             return <BlogComp blog={blog} i={i} changeLang={changeLang} />;
           })}
