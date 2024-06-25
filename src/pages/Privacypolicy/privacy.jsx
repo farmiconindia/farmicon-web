8b3946406b9e5f0de491d56dccfee96d1afb2231
farmiconindia <110238011+farmiconindia@users.noreply.github.com>
2024-06-25 12:19:53 +0530

    Add files via upload

---
+++ src/pages/Privacypolicy/privacy.jsx
@@ -0,0 +1,267 @@
+import React from 'react';
+import './privacy.scss';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="privacy-policy-container">
+      <h1>Privacy Policy</h1>
+      <div className="privacy-policy-content">
+        <div>
+          <p>Your privacy is critically important to us (Farmicon)</p>
+          <p>Welcome to Farmicon Private Limited (Farmicon)</p>
+          <address>
+            <strong>Corporate Office Address:</strong><br />
+            Farmicon India Private Limited<br />
+            I-3, Tides Incubation Centre,<br />
+            IIT Roorkee, Roorkee,<br />
+            Uttarakhand – 247667, India<br />
+            Contact: 8650505520
+          </address>
+        </div>
+        <div>
+          <p>
+            It is Farmicon's policy to respect your privacy regarding any information we may collect
+            while operating our website. This Privacy Policy applies to https://www.farmicon.in/
+            (hereinafter, "us", "we", or "https://www.farmicon.in/"). We respect your privacy and are
+            committed to protecting personally identifiable information you may provide us through
+            the Website. We have adopted this privacy policy ("Privacy Policy") to explain what
+            information may be collected on our website, how we use this information, and under
+            what circumstances we may disclose the information to third parties. This Privacy
+            Policy applies only to information we collect through the Website and does not apply to
+            our collection of information from other sources.
+          </p>
+          <p>
+            This Privacy Policy, together with the Terms and Conditions posted on our website, set
+            forth the general rules and policies governing your use of our website. Depending on
+            your activities when visiting our website, you may be required to agree to additional
+            terms and conditions.
+          </p>
+        </div>
+        <h2>Website Visitors</h2>
+        <div>
+          <p>
+            Like most website operators, Farmicon collects non-personally identifying information
+            of the sort that web browsers and servers typically make available, such as the browser
+            type, language preference, referring site, and the date and time of each visitor request.
+            Farmicon's purpose in collecting non-personally identifying information is to better
+            understand how Farmicon's visitors use its website. From time to time, Farmicon may
+            release non-personally identifying information in the aggregate, e.g., by publishing a
+            report on trends in the usage of its website.
+          </p>
+          <p>
+            Farmicon also collects potentially personally identifying information like Internet
+            Protocol (IP) addresses for logged in users and for users leaving comments on
+            https://www.farmicon.in/ blog posts. Farmicon only discloses logged in user and
+            commenter IP addresses under the same circumstances that it uses and discloses
+            personally identifying information as described below.
+          </p>
+        </div>
+        <h2>Sign Up for Our Service</h2>
+        <div>
+          <p>
+            If you sign up for our service, we will ask you for your name and phone number. The
+            purpose of the processing is to allow you access to our services and to provide the
+            services to you. The purpose is additionally to reach you with important notifications
+            and updates regarding the service.
+          </p>
+          <p>
+            <strong>Gathering Of Personally Identifying Information</strong>
+          </p>
+          <p>
+            Certain visitors to Farmicon's websites choose to interact with Farmicon in ways that
+            require Farmicon to gather personally identifying information. The amount and type of
+            information that Farmicon gathers depends on the nature of the interaction. For
+            example, we ask visitors who sign up for a blog at https://www.farmicon.in/ to provide
+            a username, email address, and phone number.
+          </p>
+        </div>
+        <h2>Security</h2>
+        <div>
+          <p>
+            The security of your Personal Information is important to us but remember that no
+            method of transmission over the Internet, or method of electronic storage is 100%
+            secure. While we strive to use commercially acceptable means to protect your
+            Personal Information, we cannot guarantee its absolute security.
+          </p>
+        </div>
+        <h2>Advertisements</h2>
+        <div>
+          <p>
+            Ads appearing on our website may be delivered to users by advertising partners, who
+            may set cookies. These cookies allow the ad server to recognize your computer each
+            time they send you an online advertisement to compile information about you or others
+            who use your computer. This information allows ad networks to, among other things,
+            deliver targeted advertisements that they believe will be of most interest to you. This
+            Privacy Policy covers the use of cookies by Farmicon and does not cover the use of
+            cookies by any advertisers.
+          </p>
+        </div>
+        <h2>Links To External Sites</h2>
+        <div>
+          <p>
+            Our Service may contain links to external sites that are not operated by us. If you click
+            on a third-party link, you will be directed to that third party's site. We strongly advise you
+            to review the Privacy Policy and terms and conditions of every site you visit.
+            We have no control over, and assume no responsibility for the content, privacy policies,
+            or practices of any third-party sites, products, or services.
+          </p>
+          <p>
+            https://www.farmicon.in Uses Google AdWords for Remarketing
+          </p>
+          <p>
+            https://www.farmicon.in/ uses the remarketing services to advertise on third-party
+            websites (including Google) to previous visitors to our site. It could mean that we
+            advertise to previous visitors who haven't completed a task on our site, for example
+            using the contact form to make an enquiry. This could be in the form of an
+            advertisement on the Google search results page, or a site in the Google Display
+            Network. Third-party vendors, including Google, use cookies to serve ads based on
+            someone's past visits. Of course, any data collected will be used in accordance with
+            our own privacy policy and Google's privacy policy.
+          </p>
+          <p>
+            You can set preferences for how Google advertises to you using the Google Ad
+            Preferences page, and if you want to, you can opt out of interest-based advertising
+            entirely by cookie settings or permanently using a browser plugin.
+          </p>
+        </div>
+        <h2>Protection Of Certain Personally Identifying Information</h2>
+        <div>
+          <p>
+            Farmicon discloses potentially personally identifying and personally identifying
+            information only to those of its employees, contractors, and affiliated organizations
+            that (i) need to know that information in order to process it on Farmicon's behalf or to
+            provide services available at Farmicon's website, and (ii) that have agreed not to
+            disclose it to others. Some of those employees, contractors, and affiliated
+            organizations may be located outside of your home country; by using Farmicon's
+            website, you consent to the transfer of such information to them. Farmicon will not rent
+            or sell potentially personally identifying and personally identifying information to
+            anyone. Other than to its employees, contractors, and affiliated organizations, as
+            described above, Farmicon discloses potentially personally-identifying and personally-
+            identifying information only in response to a subpoena, court order, or other
+            governmental request, or when Farmicon believes in good faith that disclosure is
+            reasonably necessary to protect the property or rights of Farmicon, third parties, or the
+            public at large.
+          </p>
+          <p>
+            If you are a registered user of https://www.farmicon.in/ and have supplied your email
+            address, Farmicon may occasionally send you an email to tell you about new features,
+            solicit your feedback, or just keep you up to date with what's going on with Farmicon
+            and our products. We primarily use our blog to communicate this type of information,
+            so we expect to keep this type of email to a minimum. If you send us a request (for
+            example via a support email or via one of our feedback mechanisms), we reserve the
+            right to publish it in order to help us clarify or respond to your request or to help us
+            support other users. Farmicon takes all measures reasonably necessary to protect
+            against the unauthorized access, use, alteration, or destruction of potentially
+            personally identifying and personally identifying information.
+          </p>
+        </div>
+        <h2>Aggregated Statistics</h2>
+        <div>
+          <p>
+            Farmicon may collect statistics about the behavior of visitors to its website. Farmicon
+            may display this information publicly or provide it to others. However, Farmicon does
+            not disclose your personally identifying information.
+          </p>
+        </div>
+        <h2>Cookies</h2>
+        <div>
+          <p>
+            To enrich and perfect your online experience, Farmicon uses "Cookies", similar
+            technologies, and services provided by others to display personalized content,
+            appropriate advertising, and store your preferences on your computer.
+          </p>
+          <p>
+            A cookie is a string of information that a website stores on a visitor's computer, and that
+            the visitor's browser provides to the website each time the visitor returns. Farmicon
+            uses cookies to help Farmicon identify and track visitors, their usage of
+            https://www.farmicon.in/, and their website access preferences. Farmicon visitors
+            who do not wish to have cookies placed on their computers should set their browsers
+            to refuse cookies before using Farmicon's websites, with the drawback that certain
+            features of Farmicon's websites may not function properly without the aid of cookies.
+          </p>
+          <p>
+            By continuing to navigate our website without changing your cookie settings, you hereby
+            acknowledge and agree to Farmicon's use of cookies.
+          </p>
+        </div>
+        <h2>Business Transfers</h2>
+        <div>
+          <p>
+            If Farmicon, or substantially all of its assets, were acquired, or in the unlikely event that
+            Farmicon goes out of business or enters bankruptcy, user information would be one of
+            the assets that is transferred or acquired by a third party. You acknowledge that such
+            transfers may occur, and that any acquirer of Farmicon may continue to use your
+            personal information as set forth in this policy.
+          </p>
+        </div>
+        <h2>Privacy Policy Changes</h2>
+        <div>
+          <p>
+            Although most changes are likely to be minor, Farmicon may change its Privacy Policy
+            from time to time, and in Farmicon's sole discretion. Farmicon encourages visitors to
+            frequently check this page for any changes to its Privacy Policy. Your continued use of
+            this site after any change in this Privacy Policy will constitute your acceptance of such
+            change.
+          </p>
+        </div>
+        <h2>Location Data</h2>
+        <div>
+          <p>
+            We may use and store information about your location if you give us permission to do
+            so ("Location Data"). We use this data to provide features of our Service, to improve and
+            customize our Service.
+          </p>
+          <p>
+            You can enable or disable location services when you use our Service at any time by
+            way of your device settings.
+          </p>
+        </div>
+        <h2>Grievance Officer</h2>
+        <div>
+          <p>
+            In accordance with The Information Technology Act, 2000 and rules made thereunder,
+            the name and contact details of the Grievance Officer are provided below. Should you
+            have any queries, discrepancies, and grievances regarding the collection or usage of
+            your personal information, you may contact:
+          </p>
+          <address>
+            Name: Grievance Officer, Farmicon Private Limited<br />
+            Address: I-3, Tides Incubation Centre, IIT Roorkee, Roorkee, Uttarakhand- 247667, India<br />
+            Phone: +91 8650505520
+          </address>
+          <p>
+            If you do not wish to receive any communications from us, then you may exercise your
+            opt-out rights by communicating to the Grievance Officer about the deletion of your
+            personal details shared earlier. Post receiving such communication, we will not use or
+            share your information with anyone but we may retain certain information obtained
+            from you, as records for auditory and legal compliance purposes.
+          </p>
+        </div>
+        <h2>Privacy Excellence: Our Pledge to Privacy by Design</h2>
+        <div>
+          <p>
+            Farmicon is committed to the principles of Privacy by Design, an approach to privacy
+            that prioritizes the inclusion of privacy protections from the outset of the development of
+            our systems, products, and business practices. We adhere to the Seven Principles of
+            Privacy by Design as formulated by Dr. Ann Cavoukian:
+          </p>
+          <ol>
+            <li>Proactive not Reactive: We strive to anticipate and prevent privacy-invasive events before they occur, rather than addressing them after the fact.</li>
+            <li>Privacy as the Default: Privacy settings should be automatically set to the most private, limiting access and use of personal information to the extent necessary for the intended purpose.</li>
+            <li>Privacy Embedded into Design: We integrate privacy measures into the design and architecture of our systems, products, and processes, ensuring that privacy is an integral part of the development lifecycle.</li>
+            <li>Full Functionality: Privacy should not be sacrificed for functionality. We seek to provide full functionality without compromising privacy, adopting solutions that are positive sum, offering benefits for all stakeholders.</li>
+            <li>End-to-End Security: We implement robust security measures to protect personal information throughout its entire lifecycle, from collection to storage, use, and disposal.</li>
+            <li>Visibility and Transparency: We are transparent about our data practices, providing clear information to individuals about how their personal information is collected, used, and shared.</li>
+            <li>Respect for User Privacy: We respect user privacy and empower individuals to have control over their personal information. We seek to offer user-friendly options and choices regarding the collection and use of their data.</li>
+          </ol>
+          <p>
+            By adopting the Privacy by Design principles, Farmicon aims to enhance user privacy,
+            build trust, and demonstrate our commitment to responsible data handling practices.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
