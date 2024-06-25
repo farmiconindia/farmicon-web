a8d33b5cbde75b32d29cb23cea7dbb87d0711918
farmiconindia <110238011+farmiconindia@users.noreply.github.com>
2024-06-25 12:16:43 +0530

    Update App.jsx
    After AI and TnC

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import CropPrice from "./pages/services/CropPrice/CropPrice";
 import Drone from "./pages/services/drone/Drone";
 import Services from "./pages/services/Services";
 import "./style.scss";
+import TermsAndConditions from "./pages/Tnc/Terms";
+import PrivacyPolicy from "./pages/Privacypolicy/privacy";
 import About from "./pages/about/About";
 import Blog from "./pages/blog/Blog";
 import Weather from "./pages/weather/Weather";
@@ -16,6 +18,7 @@ import BlogPage from "./pages/blogPage/BlogPag";
 import Product from "./pages/product/Product";
 import CropHealth from "./pages/services/cropHealth/CropHealth";
 import SoilHealth from "./pages/services/soilhealth/SoilHealth";
+import Ai from "./pages/ai/aii";
 import GoToTop from './components/GoToTop/GoToTop';
 import { Analytics } from '@vercel/analytics/react';
 
@@ -46,9 +49,8 @@ function App() {
           element: <Services changeLang={changeLang} />,
         },
         {
-          path:"/product",
-          element: <Product changelang={changeLang}/>
-
+          path: "/product",
+          element: <Product changelang={changeLang} />
         },
         {
           path: "/services/cropPrice",
@@ -60,17 +62,16 @@ function App() {
         },
         {
           path: "/services/cropHealth",
-          element: <CropHealth changeLang={changeLang}/>,
+          element: <CropHealth changeLang={changeLang} />,
         },
         {
           path: "/services/soilHealth",
-          element: <SoilHealth changeLang={changeLang}/>,
+          element: <SoilHealth changeLang={changeLang} />,
         },
         {
           path: "/services/drone",
-          element: <Drone changeLang={changeLang}/>,
+          element: <Drone changeLang={changeLang} />,
         },
-        
         {
           path: "/about",
           element: <About changeLang={changeLang} />,
@@ -88,12 +89,25 @@ function App() {
           element: <Contact changeLang={changeLang} />,
         },
         {
+          path: "/ai",
+          element: <Ai changeLang={changeLang} />,
+        },
+        {
           path: "/blogs/:id",
           element: <Blog changeLang={changeLang} />,
+        },
+        {
+          path: "/terms-and-conditions",
+          element: <TermsAndConditions changeLang={changeLang} />,
+        },
+        {
+          path: "/privacy-policy",
+          element: <PrivacyPolicy changeLang={changeLang} />,
         },
       ],
     },
   ]);
+
   return (
     <>
       <RouterProvider router={router} />
